4aa671b96a3f608a5862a837d8a4894e937bed11
Cesar Landaeta <clandaeta@serv.externos.prismamp.com>
2024-05-22 16:33:08 -0300

    Ejercicios Resueltos

---
+++ EjercicioClase2Modulo2/EdicionCliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase2Modulo2
+{
+    internal class EdicionCliente
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Vip { get; set; }
+    }
+
+}
--- EjercicioClase2Modulo2/Program.cs
+++ EjercicioClase2Modulo2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EjercicioClase2Modulo2
+﻿using System.Runtime.InteropServices;
+
+namespace EjercicioClase2Modulo2
 {
     internal class Program
     {
@@ -23,6 +25,11 @@
             // Detectar cual es el numero mas grande e imprimirlo por consola
 
             var lstNumeros = new List<int>() { 25,10,99,105, 1, 84, 22};
+            var lstNumeroMayor = lstNumeros.Where(w => w > 99);
+            foreach (var numero in lstNumeroMayor)
+            {
+                Console.WriteLine("El numero mayor de la lista es: "+numero);
+            }
 
             #endregion
 
@@ -30,19 +37,36 @@
 
             //Ordenar los nombres alfabeticamente
             var lstNombres = new List<string>() { "Ana", "Alejandro", "Alexis", "Pablo", "Carlos", "Anibal", "Carla", "Susana" };
-
+            var lstOrdenAlfabetico = lstNombres.OrderBy(w => w);
+            Console.WriteLine("Nombres ordenados alfabéticamente: ");
+            foreach (var nombre in lstOrdenAlfabetico)
+            {
+                
+                Console.WriteLine(nombre);
+            }
 
             #endregion
 
             #region Ejercicio3
             // Utilizando la variable "lstClientes" filtrar los clientes que tengan vip = true e imprimirlo por consola
+            var lstClientesVip = lstClientes.Where(w => w.Vip == true );
+            Console.WriteLine("Los clientes VIP son: ");
+            foreach (var clienteVip in lstClientesVip)
+            {
+                Console.WriteLine(clienteVip.Nombre);
+            }
 
             #endregion
 
             #region Ejercicio4 
 
             //Utilizando la variable "lstClientes" crear una nueva lista donde solo se encuentren los nombres de los clientes e imprimir los nombres
-
+            var lstNombreClientes = lstClientes.Select( s => s.Nombre);
+            Console.WriteLine("El listado completo de clientes es el siguiente: ");
+            foreach (var nombres in lstNombreClientes)
+            {
+                Console.WriteLine(nombres);
+            }
             #endregion
 
             #region Ejercicio5
@@ -50,6 +74,17 @@
             // Nombre tiene que guardarse en mayusculas
             // Apellido tiene que guardarse en mayusculas
             // Vip => se debe evaluar el bool y si es true se debe asignar el texto "Premium" y si es false "Normal"
+            var lstListaModificada = lstClientes.Select(s => new EdicionCliente
+            {
+                Nombre = s.Nombre.ToUpper(),    
+                Apellido = s.Apellido.ToUpper(),
+                Vip = s.Vip ? "Premium" : "Normal"
+            }).ToList();
+            foreach (var cliente in lstListaModificada)
+            {
+                Console.WriteLine($"Nombre: {cliente.Nombre}, Apellido: {cliente.Apellido}, Vip: {cliente.Vip}");
+            }
+
             #endregion
         }
     }
